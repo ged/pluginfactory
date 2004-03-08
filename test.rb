70387ea8bc6f8cb964d56258aeb40cd7589ab197
stillflame <none@none>
2004-03-08 03:06:05 +0000

    filling in
    --HG--
    extra : convert_revision : svn%3A6e23ef46-5ce2-0310-a6d7-e5df34f24cd3/trunk%4012

---
+++ test.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby -w
+#
+#	Tests for the PluginFactory module
+#
+
+$:.unshift(File.join(File.dirname(__FILE__)), "lib")
+$:.unshift(File.join(File.dirname(__FILE__)), "tests")
+
+require "pluginfactory"
+require "test/unit"
+
+class FactoryTests < Test::Unit::TestCase
+
+
+	def test_01_inclusion 
+		assert_nothing_raised		{require "mybase"}
+		assert						MyBase
+		assert						MyBase.ancestors.include?( PluginFactory )
+	end
+
+	@@subs = %w{subof OtherSub SubOfMyBase othersubmybase deepsubof}
+
+	def test_11_creation 
+		@@subs.each {|sub|
+			result = nil
+			assert_nothing_raised		{result = MyBase.create(sub)}
+			assert						result.kind_of?(MyBase)
+			assert_match				%r[#{sub}]i, result.class.name
+		}
+	end
+
+
+end # class FactoryTests
