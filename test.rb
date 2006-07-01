5a89ccbab16f7c2a41fef5062a0917cd4d574cf8
Michael Granger <ged@FaerieMUD.org>
2006-07-01 18:50:52 +0000

    * Converted camelCased methods to under_barred ones.  * Fixed tests.
    --HG--
    extra : convert_revision : svn%3A6e23ef46-5ce2-0310-a6d7-e5df34f24cd3/trunk%4036

--- test.rb
+++ test.rb
@@ -3,11 +3,17 @@
 #	Tests for the PluginFactory module
 #
 
-$:.unshift(File.join(File.dirname(__FILE__)), "lib")
-$:.unshift(File.join(File.dirname(__FILE__)), "tests")
+BEGIN {
+	require 'pathname'
+	basedir = Pathname.new( __FILE__ ).dirname
+	
+	$:.unshift( basedir + "lib" )
+	$:.unshift( basedir + "tests" )
+}
 
 require "pluginfactory"
 require "test/unit"
+require 'mybase'
 
 class FactoryTests < Test::Unit::TestCase
 	
@@ -19,21 +25,47 @@ class FactoryTests < Test::Unit::TestCase
 		end
 	end
 
-	def test_01_inclusion 
-		assert_nothing_raised		{require "mybase"}
-		assert						MyBase
-		assert						MyBase.ancestors.include?( PluginFactory )
+	def test_inclusion_of_mixin_should_add_a_create_class_method
+		subclass = rval = nil
+
+		assert_nothing_raised do
+			subclass = Class::new { include PluginFactory }
+		end
+
+		assert_respond_to subclass, :create
 	end
+
 
 	@@subs = %w{subof OtherSub SubOfMyBase othersubmybase deepsubof}
 
-	def test_11_creation 
-		@@subs.each {|sub|
+
+	def test_create_should_fetch_search_dirs_from_base_class
+		subclass = rval = nil
+
+		assert_nothing_raised do
+			subclass = Class::new {
+				include PluginFactory
+				@method_called = false
+
+				class << self
+					attr_accessor :method_called
+				end
+				def self.derivative_dirs
+					method_called = true
+				end
+			}
+		end
+
+		
+	end
+
+	def test_creation 
+		@@subs.each do |sub|
 			result = nil
 			assert_nothing_raised		{result = MyBase.create(sub)}
 			assert						result.kind_of?(MyBase)
 			assert_match				%r[#{sub}]i, result.class.name
-		}
+		end
 	end
 
 
